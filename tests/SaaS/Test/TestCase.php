8f7906f23ba887cc53b6553eb3e6303e3bfc4eb9
Alex Lushpai <lushpai@gmail.com>
2016-11-23 02:22:25 +0300

    InPost methods, tests, annotations

--- tests/SaaS/Test/TestCase.php
+++ tests/SaaS/Test/TestCase.php
@@ -54,11 +54,11 @@ class TestCase extends \PHPUnit_Framework_TestCase
      *
      * @param string $token token
      * @param string $url   internal url
-     * 
+     *
      *
      * @return TiuApi
      */
-    public static function getTiuApiClient( 
+    public static function getTiuApiClient(
         $token = null,
         $url = null
     ) {
@@ -71,7 +71,7 @@ class TestCase extends \PHPUnit_Framework_TestCase
     /**
      * Get InPost API clent
      *
-     * @return InpostApi
+     * @return InPostApi
      */
     public static function getInpostApiClient()
     {
