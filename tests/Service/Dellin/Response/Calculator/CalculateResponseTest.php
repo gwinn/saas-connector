8ab26adad1086657fe3ca703cd6a3d6ef171bc67
Ruslan Efanov <rus.efanov@bk.ru>
2022-08-02 18:47:00 +0300

    add model of OrderDates for v2 calculate

--- tests/Service/Dellin/Response/Calculator/CalculateResponseTest.php
+++ tests/Service/Dellin/Response/Calculator/CalculateResponseTest.php
@@ -54,14 +54,28 @@ class CalculateResponseTest extends TestCase
         static::assertEquals(2150.0, $calculateResponse->data->arrival->price);
         static::assertEquals(2100.0, $calculateResponse->data->arrival->servicePrice);
         static::assertEquals("auto", $calculateResponse->data->priceMinimal);
-        static::assertNotEmpty($calculateResponse->data->orderDates->pickup);
-        static::assertNotEmpty($calculateResponse->data->orderDates->arrivalToOspReceiver);
-        static::assertNotEmpty($calculateResponse->data->orderDates->derrivalFromOspReceiver);
         static::assertEquals(2500.0, $calculateResponse->data->availableDeliveryTypes->auto);
         static::assertEquals(1500.0, $calculateResponse->data->availableDeliveryTypes->avia);
 
         foreach (["intercity", "air", "express", "letter", "small"] as $tariff) {
             static::assertNotEmpty($calculateResponse->data->{$tariff}->price);
         }
+
+        $orderDates = $calculateResponse->data->orderDates;
+
+        self::assertEquals('2019-04-24', $orderDates->pickup);
+        self::assertEquals('18:00:00', $orderDates->senderAddressTime);
+        self::assertEquals('15:00:00', $orderDates->senderTerminalTime);
+        self::assertEquals('2019-04-24', $orderDates->arrivalToOspSender);
+        self::assertEquals('2019-04-24', $orderDates->derivalFromOspSender);
+        self::assertEquals('2019-04-24', $orderDates->arrivalToOspReceiver);
+        self::assertEquals('2019-04-25', $orderDates->arrivalToAirport);
+        self::assertEquals('2019-04-26', $orderDates->arrivalToAirportMax);
+        self::assertEquals('2019-04-25 00:00:00', $orderDates->giveoutFromOspReceiver);
+        self::assertEquals('2019-04-26 00:00:00', $orderDates->giveoutFromOspReceiverMax);
+        self::assertEquals('2019-04-26', $orderDates->derivalFromOspReceiver);
+        self::assertEquals('14:00:00', $orderDates->createTo);
+        self::assertEquals('2022-08-05 10:00:00', $orderDates->derivalToAddress);
+        self::assertEquals('2022-08-05 17:00:00', $orderDates->derivalToAddressMax);
     }
 }
